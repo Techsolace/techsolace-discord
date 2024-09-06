142781e6d4a7059bfee8797a3da0bc2c87f220b8
Samarth <samarplayz1337@gmail.com>
2024-09-06 23:40:57 +0530

    added pynacl

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ bot.owner_ids = [1139950107995934863, 680820132355899622, 1148259965946052619]
 
 @bot.event
 async def on_ready():
+    channel = bot.get_channel(1248259219644354572)
+    await channel.connect(reconnect=True)
     for files in os.listdir('./cogs'):
         if files.endswith(".py"):
             try:
