8d858821878e6b1e4bc1a88e1af9b98aa19db21a
Samarth <samarplayz1337@gmail.com>
2024-10-10 10:54:30 +0530

    completed the tasks validation also added the start task loop

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 class Tasks(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.update_server_stats.start()
 
     @tasks.loop(seconds=180)
     async def update_server_stats(self):
@@ -11,6 +12,11 @@ class Tasks(commands.Cog):
         if guild is not None:
             channel = guild.get_channel(1292620756630507560)
             await channel.edit(name=f"members : {guild.member_count}")
+
+    @update_server_stats.before_loop
+    async def before_update_server_stats(self):
+        await self.bot.wait_until_ready()
+
 
 async def setup(bot):
     await bot.add_cog(Tasks(bot))
