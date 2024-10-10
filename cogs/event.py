b0f2ba63c79f1d2874d0ba64585852371bddf25c
Samarth <samarplayz1337@gmail.com>
2024-10-10 10:27:43 +0530

    added boost event

--- cogs/event.py
+++ cogs/event.py
@@ -1,3 +1,4 @@
+import config
 import discord
 import random
 from discord.ext import commands
@@ -15,6 +16,23 @@ class Events(commands.Cog):
             await channel.send(f"{emote} Hey {member.mention}, Welcome to {member.guild.name}.")
         else:
             await channel.send(f"👋 Hey {member.mention}, Welcome to {member.guild.name}.")
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        premium_channel = self.bot.get_channel(1248447678187049065)
+        if before.guild.premium_subscriber_role in after.roles:
+            emotes = after.guild.emojis
+            emote = random.choice(emotes)
+            embed = discord.Embed(
+                color=config.color,
+                description=f'{after.guild.name} currently has {len(after.guild.premium_subscribers)} boosters!,\n{len(after.guild.premium_tier)} boost level! and\n{len(after.guild.premium_subscription_count)} total boosts!'
+            )
+            embed.set_author(
+                name=after.name,
+                icon_url=after.avatar.url
+            )
+            embed.set_footer(text=f"{after.name} just boosted the server!")
+            await premium_channel.send(f"{emote} Thankyou, {after.mention} for boosting {after.guild.name}", embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Events(bot))
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-color = 0x2b2d31
+color = 0x294d8b
 
 class Media:
     google = "https://static-00.iconduck.com/assets.00/google-icon-2048x2048-pks9lbdv.png"
