ffe330b0a571cb34e7d45405b9930b1213d3100e
Samarth <samarplayz1337@gmail.com>
2024-11-11 03:23:51 +0530

    removed tasks

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -6,16 +6,7 @@ class Tasks(commands.Cog):
         self.bot = bot
         self.update_server_stats.start()
 
-    @tasks.loop(seconds=180)
-    async def update_server_stats(self):
-        guild = self.bot.get_guild(1237816968443334740)
-        if guild is not None:
-            channel = guild.get_channel(1292620756630507560)
-            await channel.edit(name=f"members : {guild.member_count}")
 
-    @update_server_stats.before_loop
-    async def before_update_server_stats(self):
-        await self.bot.wait_until_ready()
 
 
 async def setup(bot):
